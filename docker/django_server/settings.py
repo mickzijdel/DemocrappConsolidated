7972bd6b55708a0a5e9f2e6db25a10dd47fd8896
Mick Zijdel <mickzijdel@hotmail.com>
2024-03-12 09:38:31 +0000

    Add democrappbackup.bedlamtheatre.co.uk as an allowed host

--- docker/django_server/settings.py
+++ docker/django_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.56', 'democrapp.bedlamtheatre.co.uk', 'web', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.56', 'democrapp.bedlamtheatre.co.uk', 'democrappbackup.bedlamtheatre.co.uk', 'web', 'localhost', '127.0.0.1']
 
 
 # Application definition
