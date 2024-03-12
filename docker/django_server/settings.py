a446a00994c24b9e5df052e1887dde6911e40f1d
Mick Zijdel <mickzijdel@hotmail.com>
2024-03-12 08:22:14 +0000

    Briefly turn debug to True

--- docker/django_server/settings.py
+++ docker/django_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.0.56', 'democrapp.bedlamtheatre.co.uk', 'web', 'localhost', '127.0.0.1']
 
