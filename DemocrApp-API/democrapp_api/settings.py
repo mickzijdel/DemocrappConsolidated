a6331914f087446505e8758fcf1c191487f48231
Mick Zijdel <mickzijdel@hotmail.com>
2024-03-12 22:43:41 +0000

    Remove websocketmiddleware

--- DemocrApp-API/democrapp_api/settings.py
+++ DemocrApp-API/democrapp_api/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'channels.middleware.WebSocketMiddleware',
 ]
 if DEBUG:
     MIDDLEWARE.append('democrapp_api.middleware.DevProxyMiddleware')
--- docker/django_server/settings.py
+++ docker/django_server/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'channels.middleware.WebSocketMiddleware',
 ]
 
 CSRF_USE_SESSIONS = True
